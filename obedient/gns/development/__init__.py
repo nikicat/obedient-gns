a6c807e062fffba9fed5c6c6e044aa21110266fe
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-06-25 14:31:26 +0400

    package splitted to gns.base, gns.development and gns.sysmon

---
+++ obedient/gns/development/__init__.py
@@ -0,0 +1,26 @@
+from .. import base
+
+from dominator import *
+from obedient import exim
+from obedient import zookeeper
+
+
+def create():
+    ships = [LocalShip()]
+    zookeepers = zookeeper.create(ships)
+    mtas = exim.create(ships)
+    builder = base.builder(
+        zookeepers=zookeepers,
+        mtas=mtas,
+        threads=1,
+    )
+    gns = builder.build(ships)
+
+    return zookeepers + mtas + gns
+
+
+def create_reinit():
+    ship = LocalShip()
+    zookeepers = zookeeper.create([ship])
+    reinit = base.builder(zookeepers=zookeepers).reinit(ship)
+    return zookeepers + [reinit]
