a26fb0b67a7b1d137824631ad59b24abb8b805a8
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-07-15 22:23:46 +0400

    package layout reverted: all files are moved to obedient.gns

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='obedient.gns',
-        version='1.0',
+        version='2.0',
         url='https://github.com/yandex-sysmon/obedient-gns',
         license='GPLv3',
         author='Nikolay Bryskin',
         author_email='devel.niks@gmail.com',
         description='GNS obedient for Dominator',
         platforms='linux',
-        packages=['obedient.gns.base', 'obedient.gns.development'],
-        namespace_packages=['obedient', 'obedient.gns'],
-        package_data={'obedient.gns.base': ['logging.yaml', 'post-receive', 'run.sh', 'sshd_config']},
+        packages=['obedient.gns'],
+        namespace_packages=['obedient', 'obedient'],
+        package_data={'obedient.gns': ['logging.yaml', 'post-receive', 'run.sh', 'sshd_config']},
         install_requires=[
-            'dominator >=3, <4',
+            'dominator >=4, <5',
             'obedient.zookeeper',
+            'obedient.exim',
         ],
     )
