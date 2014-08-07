0fb22004f25c719eedf5246cd0645b47d0afa5e1
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-08-07 14:49:59 +0400

    renamed obedient/gns to obedient/powny

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ import setuptools
 
 if __name__ == '__main__':
     setuptools.setup(
-        name='obedient.gns',
+        name='obedient.powny',
         version='2.0',
-        url='https://github.com/yandex-sysmon/obedient-gns',
+        url='https://github.com/yandex-sysmon/obedient.powny',
         license='GPLv3',
         author='Nikolay Bryskin',
         author_email='devel.niks@gmail.com',
-        description='GNS obedient for Dominator',
+        description='Powny obedient for Dominator',
         platforms='linux',
-        packages=['obedient.gns'],
-        namespace_packages=['obedient', 'obedient'],
-        package_data={'obedient.gns': ['logging.yaml', 'post-receive', 'run.sh', 'sshd_config']},
+        packages=['obedient.powny'],
+        namespace_packages=['obedient'],
+        package_data={'obedient.powny': ['logging.yaml', 'post-receive', 'run.sh', 'sshd_config']},
         install_requires=[
             'dominator >=4, <5',
             'obedient.zookeeper',
