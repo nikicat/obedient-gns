f4787e9e701f578460f03a61b681781fb1688547
Devaev Maxim <mdevaev@gmail.com>
2014-09-30 16:10:56 +0400

    extra_scripts instead of extra_packages

--- obedient/powny/__init__.py
+++ obedient/powny/__init__.py
@@ -52,7 +52,7 @@ def make_builder(
     userapi_port=7887,
     dataapi_port=7888,
     elasticsearch_urls=(),
-    extra_packages=(),
+    extra_scripts=(),
     helpers=None,
     pownyversion='latest',
 ):
@@ -113,11 +113,8 @@ def make_builder(
             'mv pypy* /opt/pypy3',
             'curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py 2>/dev/null | pypy',
             'easy_install pip==1.4.1',
-            'pip install elog powny{versfx} {pkgs}'.format(
-                versfx=('' if pownyversion == 'latest' else '=={}'.format(pownyversion)),
-                pkgs=" ".join(extra_packages),
-            ),
-        ],
+            'pip install elog powny{}'.format('' if pownyversion == 'latest' else '=={}'.format(pownyversion)),
+        ] + list(extra_scripts),
         volumes={
             'config': config_volume.dest,
             'rules': rules.dest,
